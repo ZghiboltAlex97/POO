31b01076f30ba14800479b212e9a6a5d6ac88b56
ZghiboltAlex97 <56110917+ZghiboltAlex97@users.noreply.github.com>
2020-06-10 11:09:10 +0300

    Update Program.cs

--- AbstractPoint/AbstractPoint/Program.cs
+++ AbstractPoint/AbstractPoint/Program.cs
@@ -10,11 +10,11 @@ namespace AbstractPoint
     {
         static void Main(string[] args)
         {
-            Point d1 = new Point(PointRepresentation.Rectangular, 2, 2);
-            Point d2 = new Point(PointRepresentation.Polar, 5, 2);
-            d1.Move(5,5);
-            d1.Rotate(2);
-            d2.Move(0, -5);
+            Point d1 = new Point(PointRepresentation.Rectangular, 3, 3);
+            Point d2 = new Point(PointRepresentation.Polar, 6, 3);
+            d1.Move(6,6);
+            d1.Rotate(3);
+            d2.Move(0, -6);
             Console.WriteLine(d1);
             Console.WriteLine(d2);
             Console.ReadKey();
