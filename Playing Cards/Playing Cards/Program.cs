9cf80b1a5782ba1a8040540dc1df84bb41369df3
ZghiboltAlex97 <56110917+ZghiboltAlex97@users.noreply.github.com>
2020-06-10 12:47:43 +0300

    Add files via upload

---
+++ Playing Cards/Playing Cards/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Playing_Cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card c = new Card(rank.ace, suit.spade);
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+        public enum suit
+        {
+            spade, heart, diamond, club
+        }
+        public enum rank
+        {
+            ace, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king
+        }
+
+        public class Card
+        {
+            private readonly rank Rank;
+            private readonly suit Suit;
+            public Card(rank Rank, suit Suit)
+            {
+                this.Rank = Rank;
+                this.Suit = Suit;
+            }
+            public override string ToString()
+            {
+                return "Card: " + Rank + " of " + Suit;
+            }
+
+        }
+    }
+}
